edb0729320ab1d0821bdf21f36793538d6856fe0
Gorav Gumber <163832113+goravgumber@users.noreply.github.com>
2026-06-14 12:40:48 +0530

    added vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   cloudflare: false,
+  tanstackStart: {
+    server: {
+      preset: 'vercel'
+    }
+  },
   vite: {
     server: {
       port: 3001,
